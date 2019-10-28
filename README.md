4d7cadf678fc8ca78115365d50abdeb2fd6ee6b4
Bohdan Danishevsky <dbn@aminis.com.ua>
2019-10-29 00:56:19 +0200

    add integration with Travis-CI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # yabtool
 Yet Another Backup Tool
+
+TBD long description is required to provide information about flow
+configuration
--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ except ImportError:  # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 
-PACKAGE_NAME = "dupfilesremover"
+PACKAGE_NAME = "yabtool"
 
 README_FILE = "README.md"
 install_reqs = parse_requirements("requirements.txt", session=False)
 
 requirements = [str(ir.req) for ir in install_reqs if ir and ir.req and str(ir.req).strip()]
 hyphen_package_name = PACKAGE_NAME.replace("_", "-")
-LONG_DESCTIPTION = "Please read https://github.com/JFF-Bohdan/dupfilesremover/blob/master/README.md for information"
+LONG_DESCTIPTION = "Please read https://github.com/JFF-Bohdan/yabtool/blob/master/README.md for information"
 
 
 def read_file_content(file_name):
@@ -40,17 +40,12 @@ if __name__ == "__main__":
         long_description_content_type="text/x-rst",
         author="Bohdan Danishevsky",
         author_email="dbn@aminis.com.ua",
-        url="https://github.com/JFF-Bohdan/dupfilesremover",
-        keywords=["duplicates remover", "duplicate images remover", "dups remover"],
+        url="https://github.com/JFF-Bohdan/yabtool",
+        keywords=["flexible backup tool", "configurable multipurpose backup tool", "backup tool"],
         setup_requires=["pytest-runner"],
         tests_require=["pytest"],
         install_requires=requirements,
         classifiers=[],
         license="MIT",
-        zip_safe=False,
-        entry_points={
-            "console_scripts": [
-                "dupfilesremover=dupfilesremover.command_line.dupfilesremover:main"
-            ],
-        }
+        zip_safe=False
     )
