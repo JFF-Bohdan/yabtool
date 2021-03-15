a12cd8048143359161e7cb2f42048282c87403dc
Bohdan Danishevsky <dbn@aminis.com.ua>
2021-03-15 00:50:24 +0000

    Version 0.0.9.5: running subprocess commands as shell commands by default

--- yabtool/supported_steps/shared.py
+++ yabtool/supported_steps/shared.py
@@ -3,8 +3,8 @@ import subprocess
 
 class ThirdPartyCommandsExecutor(object):
     @staticmethod
-    def execute(command):
-        result = subprocess.run(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    def execute(command, shell: bool = True):
+        result = subprocess.run(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=shell)
 
         result.stdout = result.stdout if result.stdout is not None else bytes()
         result.stderr = result.stderr if result.stderr is not None else bytes()
--- yabtool/version.py
+++ yabtool/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.9.4"
+__version__ = "0.0.9.5"
 __app_name__ = "yabtool"
 __human_readable_app_name__ = "Yet Another Backup Tool"
 __package_description__ = (
